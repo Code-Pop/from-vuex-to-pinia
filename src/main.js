10ef33d6f4ddb90099cd022e31524e3c1835fbe1
Ben Hong <ben@bencodezen.io>
2022-03-18 11:30:08 -0700

    config: add pinia to the vue app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import store from './stores'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(createPinia()).use(router).mount('#app')
