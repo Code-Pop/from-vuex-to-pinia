c2e4854b5099f2b0441705e7686ad17146a6a928
Ben Hong <ben@bencodezen.io>
2022-03-18 11:53:14 -0700

    feature: add pinia stores for events and users

---
+++ src/stores/EventStore.js
@@ -0,0 +1,3 @@
+import { defineStore } from 'pinia'
+
+export const useEventStore = defineStore('EventStore', {})
---
+++ src/stores/UserStore.js
@@ -0,0 +1,3 @@
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('UserStore', {})
