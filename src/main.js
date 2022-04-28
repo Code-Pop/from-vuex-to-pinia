f57fc799d92fcf0ea8272d5eb930aea99a691923
Ben Hong <ben@bencodezen.io>
2022-04-28 16:14:14 -0700

    refactor: remove Vuex as a dependency from the project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import store from './stores'
 
-createApp(App).use(store).use(createPinia()).use(router).mount('#app')
+createApp(App).use(createPinia()).use(router).mount('#app')
--- src/stores/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'vuex'
-import EventService from '../services/EventService.js'
-
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {}
-})
