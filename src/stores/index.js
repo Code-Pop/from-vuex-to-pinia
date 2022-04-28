196492efd227a2cff567e763380860dd24ca9cea
Ben Hong <ben@bencodezen.io>
2022-04-28 15:08:21 -0700

    refactor: enforce consistent promise type return

--- src/stores/index.js
+++ src/stores/index.js
@@ -30,19 +30,14 @@ export default createStore({
           throw error
         })
     },
-    fetchEvent({ commit, state }, id) {
-      const existingEvent = state.events.find(event => event.id === id)
-      if (existingEvent) {
-        commit('SET_EVENT', existingEvent)
-      } else {
-        return EventService.getEvent(id)
-          .then(response => {
-            commit('SET_EVENT', response.data)
-          })
-          .catch(error => {
-            throw error
-          })
-      }
+    fetchEvent({ commit }, id) {
+      return EventService.getEvent(id)
+        .then(response => {
+          commit('SET_EVENT', response.data)
+        })
+        .catch(error => {
+          throw error
+        })
     }
   },
   modules: {}
